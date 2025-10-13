a25b48fbae6cf87c124f37f826127c5d23367c08
Linus Degen <linus132652@gmx.ch>
2025-10-13 14:27:24 +0200

    Reconnect to wifi and broker when disconnected

--- esp32-c6-sketch/esp32-c6-sketch.ino
+++ esp32-c6-sketch/esp32-c6-sketch.ino
@@ -19,6 +19,32 @@ int        port     = 1883;
 Adafruit_SGP40 sgp;
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 
+void connect_wifi() {
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("You're connected to the network");
+  Serial.println();
+}
+
+
+void connect_broker() {
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+  while (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    delay(1000);
+  }
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+}
+
+
 void setup() {
   Serial.begin(115200);
   if (Serial) {
@@ -40,31 +66,20 @@ void setup() {
   }
   Serial.println("Found SHT41 sensor");
 
-  Serial.print("Attempting to connect to WPA SSID: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  Serial.println("You're connected to the network");
-  Serial.println();
-
-  Serial.print("Attempting to connect to the MQTT broker: ");
-  Serial.println(broker);
-
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-    while (1);
-  }
-
-  Serial.println("You're connected to the MQTT broker!");
-  Serial.println();
+  connect_wifi();
+  connect_broker();
 }
 
 void loop() {
+  // Reconnect to WiFi if disconnected in meantime
+  if(WiFi.status() != WL_CONNECTED) {
+    connect_wifi();
+  }
+  // Reconnect to MQTT Broker if disconnected
+  if(!mqttClient.connected()) {
+    connect_broker();
+  }
+
   sensors_event_t humidity, temp;
   uint16_t sraw;
   int32_t voc_index;
@@ -96,5 +111,5 @@ void loop() {
   mqttClient.print(jsonBuffer);
   mqttClient.endMessage();
 
-  delay(1 * 1000);
+  delay(3 * 1000);
 }
